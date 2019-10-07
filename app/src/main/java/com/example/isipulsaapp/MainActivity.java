735b3e6442c60511b350e5f17a1f9c411ab0ccbc
path71nder <sevillebayuaji@gmail.com>
2019-10-07 20:21:03 +0700

    Project Pengisian Pulsa: Menambah UI beberapa Button

--- app/src/main/java/com/example/isipulsaapp/MainActivity.java
+++ app/src/main/java/com/example/isipulsaapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.isipulsaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void StartPengisianPulsa(View view) {
+        Intent intent = new Intent(this, PengisianPulsa.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/isipulsaapp/activity/PengisianPulsaActivity.java
@@ -0,0 +1,16 @@
+package com.example.isipulsaapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.isipulsaapp.R;
+
+public class PengisianPulsaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengisian_pulsa);
+    }
+}
